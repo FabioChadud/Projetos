6ef94a48fc6c9d83cf1165c06d7c4ee83702a177
Fabio <fabiochd@yahoo.com.br>
2025-01-10 21:40:04 -0300

    02

--- Parte 03/Aula01.py
+++ Parte 03/Aula01.py
@@ -7,11 +7,20 @@
 # n3 = int(input('informe o terceiro número:  '))
 # n4 = int(input('informe o quarto número:  '))
 
+#Exercício 01
 # media = ((n1*1)+(n2*2)+(n3*3)+(n4*4))/(1+2+3+4)
 # print('Média ponderada é ', media)
 
-dolar = float(input('Qual a contação do dolar:  '))
-quantidade = int(input('quatidade de dolares guardado:  '))
-valor = dolar*quantidade
+# Exercício 02
+# dolar = float(input('Qual a contação do dolar:  '))
+# quantidade = int(input('quatidade de dolares guardado:  '))
+# valor = dolar*quantidade
+# print('O valor no cofre é de ', valor)
 
-print('O valor no cofre é de ', valor)
+valorcompra = float(input('Qual o valor da compra?  '))
+parcela = valorcompra / 5
+print('Sua parcela é de  ', parcela)
+
+custo = float(input('Qual o preço de custo?  '))
+percentual = float(input('Qual o percentual de lucro? '))
+print('O valor do seu produto é ', (custo+(custo*(percentual/100))))
