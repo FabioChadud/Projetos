8620b88b3046786e7f1143bf355e07252caaf790
Fabio <fabiochd@yahoo.com.br>
2025-01-10 21:22:27 -0300

    aula 1

---
+++ Parte 03/Aula01.py
@@ -0,0 +1,17 @@
+# print('test')
+# numero=4
+# print('quadrado de',numero,'=',numero**2,sep='_')
+# exemplo 01
+# n1 = int(input('informe o primeiro número:  '))
+# n2 = int(input('informe o segundo número:   '))
+# n3 = int(input('informe o terceiro número:  '))
+# n4 = int(input('informe o quarto número:  '))
+
+# media = ((n1*1)+(n2*2)+(n3*3)+(n4*4))/(1+2+3+4)
+# print('Média ponderada é ', media)
+
+dolar = float(input('Qual a contação do dolar:  '))
+quantidade = int(input('quatidade de dolares guardado:  '))
+valor = dolar*quantidade
+
+print('O valor no cofre é de ', valor)
